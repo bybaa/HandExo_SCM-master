1dd56b99b08fddd7c035d1bad7031975f4584520
byb <a1282319520@163.com>
2025-06-12 15:13:14 +0800

    ip 192.168.1.30 port 1111

--- Middlewares/lwip/lwip_app/lwip_demo.c
+++ Middlewares/lwip/lwip_app/lwip_demo.c
@@ -63,7 +63,7 @@ void lwip_demo(void)
 	extern float angle_udp[18];
 	extern float ad7616f_data[AD7616_CHANNEL_GROUP_MAX * AD7616_CHANNEL_GROUP_NUM];
 	
-	float udp_data[16];
+	float udp_data[34];
 	
 	int i = 0;
     err_t err;
@@ -89,27 +89,29 @@ void lwip_demo(void)
 		
 		GetDegreeo();
 		
-		
-
         if (err == ERR_OK)                                      /* °ó¶¨Íê³É */
         {
             while (1)
             {
-//				for(i = 0;i<34;++i)
-//				{
-//					if(i < 18) 
-//					{
-//						udp_data[i] = angle_udp[i];
-//					}
-//					else 
-//					{
-//						udp_data[i] = ad7616f_data[i-18];
-//					}
-//				}
-				for (i = 0 ; i <16 ;++i)
+				for(i = 0;i<34;++i)
 				{
-					udp_data[i] = ad7616f_data[i];
+					if(i < 18) 
+					{
+						udp_data[i] = angle_udp[i];
+						
+						printf("%d: %.2f ",i,angle_udp[i]);
+					}
+					else 
+					{
+						udp_data[i] = ad7616f_data[i-18];
+					}
 				}
+				
+				printf("\n");
+//				for (i = 0 ; i <16 ;++i)
+//				{
+//					udp_data[i] = ad7616f_data[i];
+//				}
 				
 				if (1)
                 {
